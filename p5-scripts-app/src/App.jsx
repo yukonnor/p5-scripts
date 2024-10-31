62a4b4612ded92cc6d29545083fea91fb593c3a9
connor <caschm@gmail.com>
2024-10-30 18:17:44 -0700

    install mui

--- p5-scripts-app/src/App.jsx
+++ p5-scripts-app/src/App.jsx
@@ -1,34 +1,37 @@
 import { useState } from "react";
 import "./App.css";
 
+import NavBar from "./components/NavBar";
+
 import P5Wrapper from "./components/P5Wrapper";
 import redCircles from "./sketches/red-circles";
 
 function App() {
-  const [radius, setRadius] = useState(50); // Default radius
+    const [radius, setRadius] = useState(50); // Default radius
 
-  const handleRadiusChange = (e) => {
-    setRadius(e.target.value);
-  };
+    const handleRadiusChange = (e) => {
+        setRadius(e.target.value);
+    };
 
-  return (
-    <>
-      <h1>p5.js Script Sandbox</h1>
+    return (
+        <>
+            <NavBar />
+            <h1>p5.js Sandbox</h1>
 
-      <label>
-        Circle Radius: {radius}px
-        <input
-          type="range"
-          min="5"
-          max="100"
-          value={radius}
-          onChange={handleRadiusChange}
-        />
-      </label>
+            <label>
+                Circle Radius: {radius}px
+                <input
+                    type="range"
+                    min="5"
+                    max="100"
+                    value={radius}
+                    onChange={handleRadiusChange}
+                />
+            </label>
 
-      <P5Wrapper sketch={redCircles} params={{ radius }} />
-    </>
-  );
+            <P5Wrapper sketch={redCircles} params={{ radius }} />
+        </>
+    );
 }
 
 export default App;
--- p5-scripts-app/src/sketches/red-circles.js
+++ p5-scripts-app/src/sketches/red-circles.js
@@ -1,15 +1,16 @@
 const redCircles = (p) => {
-  p.setup = () => {
-    p.createCanvas(400, 400);
-    p.background(200);
-  };
+    p.setup = () => {
+        p.createCanvas(400, 400);
+        p.background(200);
+    };
 
-  p.draw = () => {
-    // Access the radius parameter dynamically, default to 50
-    const radius = p.params?.radius || 50;
-    p.fill(255, 0, 0);
-    p.ellipse(p.mouseX, p.mouseY, radius, radius);
-  };
+    p.draw = () => {
+        // Access the radius parameter dynamically, default to 50
+        const radius = p.params?.radius || 50;
+        p.fill(255, 0, 0);
+
+        p.mouseIsPressed && p.ellipse(p.mouseX, p.mouseY, radius, radius);
+    };
 };
 
 export default redCircles;
